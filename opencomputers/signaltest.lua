19f362c8ce6cb0c19f6731a30e5774a2be506491
nzhook <git.hook@hook.net.nz>
2021-05-28 13:18:44 +1200

    Add in changes for events.lua to also print out the data inside tables

--- opencomputers/signaltest.lua
+++ opencomputers/signaltest.lua
@@ -2,7 +2,24 @@
      Created for the Youtube channel https://youtube.com/user/nzHook 2019
 ]]--
 local event = require("event")
+--local component = require("component")
+--component.modem.open(53)
+
+local function debugvar(var, depth)
+  if not depth then
+    depth = 0
+  end
+  
+  for k, v in pairs(var) do
+    print (string.rep("-", depth * 2) .. k, v)
+    if type(v) == "table" then
+      debugvar(v, depth + 1)
+    end
+  end
+end
+
 while true do
-   local a, b, c, e, f, g, h = event.pull()
-   print(a,b,c,d,e,f,g,h)
+  local e = {event.pull()}
+  print("--")
+  debugvar(e)
 end
