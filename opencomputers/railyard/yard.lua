fe83a3f73b68b42f42735cf3e2f291c9cb1b819e
James Hook <4828036+nzhook@users.noreply.github.com>
2021-09-23 19:42:57 +1200

    Updated railyard code to use computronics (ep #43)

--- opencomputers/railyard/yard.lua
+++ opencomputers/railyard/yard.lua
@@ -1,8 +1,9 @@
 --[[ Rail yard train maker
      Author: nzHook
      Created for the Youtube channel https://youtube.com/user/nzHook 2018
-     myRail Episode Showing Usage: https://www.youtube.com/watch?v=Iq13el02ips
-     
+     myRail Episode Showing Usage: Ep18 - https://www.youtube.com/watch?v=Iq13el02ips
+     myRail Episode Where Updated For Computronics: Ep43 - https://youtu.be/SisYSkjtHjg
+
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
@@ -22,28 +23,29 @@ local serialization = require("serialization")
 local filesystem = require("filesystem")
 local os = require("os")
 local computer = require("computer")
+--  Make sure cns.lua library is in the same path as this file or /lib
+local cns = require("cns")
 
 -- CHANGE THESE: There is no garentuee that OC will detect this correctly each boot
 -- This is the redstone IO that is primary
-local rsid = "5b4"
--- This is the redstone IO of the engine detector
-local rsid_engine = "7171"
--- This is the redstone IO of the cart detector
-local rsid_cart = "3c5c"
-
--- rsmax is the starting point for the number calcs, eg. if the redstone io is 1 block away then any signal for white
---   would be 15 - 1. so this value should be 14
---   NOTE: Vanilla goes to 15, this code works in base 10, so you can have a maximum of 5 blocks between
-local rsmax = 14
+local rsid = "yard_redstone"
+-- This is the redstone IO of the engine detector (recomend using cns to name the device)
+local ctid_engine = "engines_in"
+-- This is the ID of the inbound cart detector
+local ctid_cart = "carts_in"
+-- This is the ID of the redstone IO placed next to the carts
+local rsid_cart = "cart_waiting"
+-- This is the ID of the cart detector where the shunter idles (used to detect it)
+local ctid_shunter = "cart_shunting"
 
 -- The sides of the transposer
-local side_train = sides.right
-local side_chest = sides.left
+--local side_train = sides.right
+--local side_chest = sides.left
 -- The slot in the train to put the ticket
 --   in a steam engine this should be 4, in a creative its slot 1
 -- TODO: Autodetect
-local slot_train = 1
-local slot_chest = nil
+--local slot_train = 1
+--local slot_chest = nil
 --- the sides and colours of the redstone io
 local side_track = sides.top
 local side_isengine = sides.right
@@ -56,12 +58,25 @@ local side_Dbottom = sides.front
 local side_Dtop = sides.back
 --- the sides of the redstone io for cart detector
 local side_cartdetect = sides.top
-local side_cartwaiting = sides.back
+local side_cartwaiting = sides.front
 
 -- The port that we send updates to - needs tp match comms_port on master
 local comms_port = 1234
 -- rcomm is the port we get updates back on -- needs to match rcomms_port on master
 local rcomms_port = 1235
+
+-- Sidings the carts are stored in (well the location of the tickets in the chest)
+--- TODO: Replace
+local cartloc = {}
+cartloc["wood"] = 1
+cartloc["chest"] = 2
+cartloc["basic"] = 3
+cartloc["cart"] = 3       -- alias
+cartloc["tank"] = 4
+cartloc["worldspike_admin"] = 5
+cartloc["chunk"] = 5      -- alias
+cartloc["cargo"] = 6
+cartloc["RETURN"] = 7
 
 -- TODO: 
 --  - Track where carts are stored and maybe how many are there
@@ -73,124 +88,166 @@ modem = component.proxy(modem)
 if not modem then error("Error loading modem") end
 
 -- Setup the components
-component.setPrimary("redstone", component.get(rsid))
+rs_yard = component.get(cns(rsid))
+ct_engine = component.get(cns(ctid_engine))
+ct_cart = component.get(cns(ctid_cart))
+ct_shunter = component.get(cns(ctid_shunter))
+rs_cart = component.get(cns(rsid_cart))
+
+if not rs_yard then
+		print("Could not find the primary Redstone IO (" .. rsid .. "/" .. cns(rsid) .. ") as a component")
+		os.exit()
+end
+print("yard redstone", rs_yard)
+
+if not ct_engine then
+		print("Could not find the Engine cart_detector (" .. ctid_engine .. "/" .. cns(ctid_engine) .. ") as a component")
+		os.exit()
+end
+print("engine detector", ct_engine)
+
+if not ct_cart then
+		print("Could not find the returning Cart cart_detector (" .. ctid_cart  .. "/" .. cns(ctid_cart).. ") as a component")
+		os.exit()
+end
+print("cart detector", ct_cart)
+
+if not rs_cart then
+		print("Could not find the waiting Cart Redstone IO (" .. rsid_cart  .. "/" .. cns(rsid_cart).. ") as a component")
+		os.exit()
+end
+print("cart waiting redstone", ct_cart)
+
+if not ct_shunter then
+		print("Could not find the shunting cart_detector (" .. ctid_shunter  .. "/" .. cns(ctid_shunter).. ") as a component")
+		os.exit()
+end
+print("shunter detector", ct_shunter)
+
+-- sleep for a few seconds to wait for the components to settle (otherwise it gets a no primary redstone error)
+os.sleep(2)
+component.setPrimary("redstone", rs_yard)
 modem.open(rcomms_port)
 modem.broadcast(comms_port, "yardevent", id, "boot")
 
+print("Yard starting")
 
-rsid_engine = component.get(rsid_engine)
-rsid_cart = component.get(rsid_cart)
-
-if not rsid_engine then
-		print("Could not determine Engine detector Redstone IO")
-		os.exit()
-end
-if not rsid_cart then
-		print("Could not determine Cart detector Redstone IO")
-		os.exit()
-end
+-- set the destintion to empty
+component.locomotive_relay.setDestination("")
 
 
 local nextresend = 0
-
--- Sidings the carts are stored in (well the location of the tickets in the chest)
-local cartloc = {}
-cartloc["wood"] = 1
-cartloc["chest"] = 2
-cartloc["cart"] = 3
-cartloc["tanker"] = 4
-cartloc["chunk"] = 5
-cartloc["cargo"] = 6
-cartloc["RETURN"] = 7
-
--- The mapping of redston in the cart detector to carts
---   Make sure that all the cart names listed here exist in the above array
---   In the future the above array may be dynamic, but this will need to match the detectors
-local cartmatch = {}
-cartmatch[0] = "wood"
-cartmatch[1] = "chest"
-cartmatch[2] = "cart"
-cartmatch[3] = "tanker"
-cartmatch[4] = "chunk"
-cartmatch[5] = "cargo"
 
 -- If something is waiting to be built, we no longer know about it so release it
 component.redstone.setBundledOutput(side_bundle, sidecolor_release, 15)
 component.redstone.setBundledOutput(side_bundle, sidecolor_release, 0)
 
+-- Currently computonics only provides events not a read now command
+--  so if we see a minecart event for the loco stand then we need to store what it is
+--  one issue with this is if the cart is we dont get a siganl when the cart leves so this will
+--  always be set. Could have a railcraft cart detector as well and when its no longer on we unset
+--  but that might be a bit much
+local ct_shunter_type = nil
+event.listen("minecart", function(_, deviceid, carttype) 
+        if(deviceid == ct_shunter) then
+          ct_shunter_type = carttype
+        end
+  end )
+
 
 while true do
   local step = 0
   local carts = {}
+local scounter = 0
 
 	component.redstone.setBundledOutput(side_bundle, sidecolor_decouple, 0)
 
 	-- First loop, wait for something to do
   while true do
-     local a, b, c, d, e, f, g = event.pull(2)
-     if not a then
-			if component.transposer.getInventorySize(side_train) then
-				if component.redstone.getInput(side_isengine) == 0 then
-					-- Its not the shunter - maybe a cart being pushed. let it go
-					component.redstone.setOutput(side_track, 15)
-					component.redstone.setOutput(side_track, 0)
-				else
-					-- If we have been idle for a while look to see if there
-					--  are carts to return. Returning is more important than
-					--  building because we may need one of these carts
-					if component.invoke(rsid_cart, "getInput", side_cartwaiting) > 0 then
-						print("A cart is waiting, releasing shunter to pick up")
-						component.transposer.transferItem(side_chest, side_train, slot_train, cartloc["RETURN"])
-						--os.sleep(1)
-						component.transposer.transferItem(side_train, side_chest, cartloc["RETURN"], slot_train)
-						
-						component.redstone.setOutput(side_track, 15)
-						component.redstone.setOutput(side_track, 0)
-						component.redstone.setBundledOutput(side_bundle, sidecolor_decouple, 15)
-					else
-						-- If no carts are there then release a train - doing this on the timeout is slow
-						-- but it ensures we are runniong and can detect the inbound train
-						component.redstone.setBundledOutput(side_bundle, sidecolor_next, 15)
-						component.redstone.setBundledOutput(side_bundle, sidecolor_next, 0)
-					end
-				end
-			end
-     elseif a == "redstone_changed" then 
-        print(a, b, sides[c], d, e)
-        if e > 0 then -- Ignore when the engine moves off
-          if b == rsid_engine then
-            -- What we do depends on the side
-            if sides[c] == "top" and currentT and currentB then
-              -- Engine has rolled over the final detector send the final number to the master
-            
-              print("new engine " .. currentB, currentT)
-              nextresend = computer.uptime()
-            elseif c == side_Dbottom then
-              -- The bottom colour number which we use as the base
-              currentB = rsmax - e
-            elseif c == side_Dtop then
-              -- The top colour number which we use as the base
-              currentT = rsmax - e
-            end
-					elseif b == rsid_cart and c == side_cartdetect then
-						-- Map the redstone to the cart type
-						-- TODO: Need something more dynamic here
-						returncart = cartmatch[rsmax - e]
-						print("Cart detector gave " .. (rsmax - e) .. "...")
-						print(" -> returning to " .. cartloc[cartmatch[rsmax - e]] .. " (" .. cartmatch[rsmax - e] .. ")")
-						carts = {}
-						-- TODO What happens if we dont have a matching cart location?  optimal would be for us to pick a free
-						--   siding, but we dont track that at the moment. ATM This will cause a code error
-						table.insert(carts, cartloc[cartmatch[rsmax - e]])
-						-- TODO: Enable the decoupling tracks --  and disable at stat of run (after cart is returned)
-						break
-					elseif b == rsid and not (c == side_isengine) then
-						-- Its not the shunter - maybe a cart being pushed. let it go
-						component.redstone.setOutput(side_track, 15)
-						component.redstone.setOutput(side_track, 0)
+     local a, b, c, d, e, f, g = event.pull(1)
+    if not a then
+     -- timeout
+     if component.locomotive_relay.getDestination() == "" or scounter > 0 then
+        -- If we have been idle for a while look to see if there
+        --  are carts to return. Returning is more important than
+        --  building because we may need one of these carts
+        if component.invoke(rs_cart, "getInput", side_cartwaiting) > 0 then
+          print("A cart is waiting, releasing shunter to pick up")
+          --component.transposer.transferItem(side_chest, side_train, slot_train, cartloc["RETURN"])
+          --os.sleep(1)
+          --component.transposer.transferItem(side_train, side_chest, cartloc["RETURN"], slot_train)
+          component.locomotive_relay.setDestination("R1")
+          
+          
+          component.redstone.setOutput(side_track, 15)
+          component.redstone.setOutput(side_track, 0)
+          component.redstone.setBundledOutput(side_bundle, sidecolor_decouple, 3600)
+        else
+          -- If no carts are there then release a train - doing this on the timeout is slow
+          -- but it ensures we are runniong and can detect the inbound train
+          component.redstone.setBundledOutput(side_bundle, sidecolor_next, 3600)
+          component.redstone.setBundledOutput(side_bundle, sidecolor_next, 0)
+          
+          scounter = scounter + 1
+          if scounter == 1 then
+            component.locomotive_relay.setDestination("Like")
+          elseif scounter == 2 then
+            component.locomotive_relay.setDestination("and")
+          elseif scounter == 3 then
+            component.locomotive_relay.setDestination("Subscribe")
+          elseif scounter == 4 then
+            component.locomotive_relay.setDestination("")
+          elseif scounter == 6 then
+            scounter = 0
           end
+
         end
-      elseif a == "modem_message" then
+     end
+    elseif a == "minecart" then
+      -- minecart is the computronics signal for the digital cart detector
+      -- b=address
+      -- c=type
+      -- d=name
+      -- e=primaryColor
+      -- f=secondaryColor
+      -- g=destination
+      -- h=owner
+      if b == ct_engine then
+        -- New engine has rolled in send the number to the master
+        currentT = e
+        currentB = f
+        print("new engine " .. currentB, currentT)
+        nextresend = computer.uptime()
+      elseif b == ct_cart then
+        -- Map the redstone to the cart type
+        -- TODO: Need something more dynamic here
+        if c == "locomotive_electric" or c == "locomotive_creative" then
+          -- ignore the shunter passing
+        else
+          -- TODO What happens if we dont have a matching cart location?  optimal would be for us to pick a free
+          --   siding, but we dont track that at the moment. ATM This will cause a code error
+          c = string.sub(c, 6)
+          if cartloc[c] then
+            print("Cart detector gave " .. c .. "...")
+            print(" -> returning to " .. cartloc[c])
+            carts = {}
+            table.insert(carts, cartloc[c])
+            -- TODO: Enable the decoupling tracks --  and disable at start of run (after cart is returned)
+          else
+            error("Unknown cart " .. c)
+          end
+          break
+        end
+      elseif b == ct_shunter then
+        if c == "locomotive_electric" or c == "locomotive_creative" then
+          -- Its the shunter we can let it idle
+        else
+          -- Its not the shunter - maybe a cart being pushed. let it go
+          component.redstone.setOutput(side_track, 3600)
+          component.redstone.setOutput(side_track, 0)
+        end
+     end
+     elseif a == "modem_message" then
         print("REPLY", a, b, c, d, e, f, g)
         if f == "yardbuild" then
           currentB = 0
@@ -238,14 +295,15 @@ while true do
   while true do
     component.redstone.setOutput(side_track, 0)
     local e = event.pull(2)
-		
-    if component.transposer.getInventorySize(side_train) then
-      -- Every now and then, we detect the shuter passing. so wait 1 second and then test again
-      os.sleep(1)
-      if component.transposer.getInventorySize(side_train) then
-				if component.redstone.getInput(side_isengine) == 0 then
+    
+    -- we wait for the cart detector to signal that the shunter is stalled
+    if component.redstone.getInput(side_isengine) > 2 then
+      -- Every now and then, we detect the shunter passing. so wait 1 second and then test again
+      os.sleep(2)
+      if component.redstone.getInput(side_isengine) > 2 then
+        if ct_shunter_type ~= "locomotive_electric" and ct_shunter_type ~= "locomotive_creative" then
 					-- Its not the shunter - maybe a cart being pushed. let it go
-					component.redstone.setOutput(side_track, 15)
+					component.redstone.setOutput(side_track, 3600)
 					component.redstone.setOutput(side_track, 0)
 				else
 					step = step + 1
@@ -258,13 +316,14 @@ while true do
 					print(step .. " = " .. slot_chest)
 					component.redstone.setOutput(side_track, 0)
 					if slot_chest then
-						component.transposer.transferItem(side_chest, side_train, slot_train, slot_chest)
+						--component.transposer.transferItem(side_chest, side_train, slot_train, slot_chest)
 						--os.sleep(1)
-						component.transposer.transferItem(side_train, side_chest, slot_chest, slot_train)
+						--component.transposer.transferItem(side_train, side_chest, slot_chest, slot_train)
+            component.locomotive_relay.setDestination("T" .. slot_chest)
 					end
 				
 					-- trigger the train to move
-					component.redstone.setOutput(side_track, 15)
+					component.redstone.setOutput(side_track, 3600)
 				end
       end
     end
@@ -272,7 +331,8 @@ while true do
 
 
   print("train complete")
-  component.redstone.setBundledOutput(side_bundle, sidecolor_release, 15)
+  component.redstone.setBundledOutput(side_bundle, sidecolor_release, 3600)
   os.sleep(1)
   component.redstone.setBundledOutput(side_bundle, sidecolor_release, 0)
+  component.locomotive_relay.setDestination("")
 end
